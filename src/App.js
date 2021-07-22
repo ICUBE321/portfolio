f1483602ffdbfa170179d8ea9dd79e2143028639
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2021-07-22 14:52:43 -0400

    original code

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import logo from "./images/white.jpg";
 import linkedin from "./images/linkedin-icon.png";
 import github from "./images/github-icon.png";
 
-import { HashRouter, Route, NavLink, Switch } from "react-router-dom";
+import { HashRouter, Route, NavLink, Switch, BrowserRouter } from "react-router-dom";
+import Home from "./Components/Home";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
-import Home from "./Components/Home";
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter>
       <div className="App">
         <div className="navigation">
-          <NavLink activeClassName="active" to="/" className="logo-image" exact>
+          <NavLink exact activeClassName="active" to="/portfolio" className="logo-image">
             <img src={logo} alt="logo image" />
           </NavLink>
           <div className="navigation-sub">
-            <NavLink activeClassName="active" to="/" className="item" exact>
+            <NavLink exact activeClassName="active" to="/portfolio" className="item">
               HOME
             </NavLink>
             <NavLink activeClassName="active" to="/about" className="item">
@@ -36,7 +36,7 @@ function App() {
         </div>
         <div className="main">
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/portfolio" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
             <Route path="/contact" component={Contact} />
@@ -54,7 +54,7 @@ function App() {
           </a>
         </div>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,8 @@ const Home = (props) => {
   return (
     <div className={`${styles.home} ${shared["container-div"]}`}>
       <p className={styles["intro-text"]}>
-        Hello, I'm{" "}
-        <strong className={styles.highlighted}>Izien Iremiren</strong>.
+        Hello, I'm
+        <strong className={styles.highlighted}> Izien Iremiren</strong>.
       </p>
       <p className={styles["intro-text"]}>I'm a full-stack web developer.</p>
       <NavLink to="/projects" className={styles.projectsLink}>
@@ -17,5 +17,5 @@ const Home = (props) => {
     </div>
   );
 };
-
+  
 export default Home;
