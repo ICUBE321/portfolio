0ef876d5c5338b667d87329929e6b0e26f5dd01a
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2021-07-22 16:10:05 -0400

    works

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import logo from "./images/white.jpg";
 import linkedin from "./images/linkedin-icon.png";
 import github from "./images/github-icon.png";
 
-import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import { HashRouter as Router, Route, NavLink, useLocation } from "react-router-dom";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
 import Home from "./Components/Home";
 
 function App() {
-  // const location = useLocation();
+  const location = useLocation();
   return (
     <div className="App">
       <Router>
