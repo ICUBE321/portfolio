8110a064ca98763defb8c8e4866f48ab5fa58abf
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2021-07-22 14:31:52 -0400

    please work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from "./images/white.jpg";
 import linkedin from "./images/linkedin-icon.png";
 import github from "./images/github-icon.png";
 
-import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import { HashRouter, Route, NavLink, Switch } from "react-router-dom";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
@@ -13,24 +13,14 @@ import Home from "./Components/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <div className="App">
         <div className="navigation">
-          <NavLink
-            activeClassName="active"
-            to="/portfolio"
-            className="logo-image"
-            exact
-          >
+          <NavLink activeClassName="active" to="/" className="logo-image" exact>
             <img src={logo} alt="logo image" />
           </NavLink>
           <div className="navigation-sub">
-            <NavLink
-              activeClassName="active"
-              to="/portfolio"
-              className="item"
-              exact
-            >
+            <NavLink activeClassName="active" to="/" className="item" exact>
               HOME
             </NavLink>
             <NavLink activeClassName="active" to="/about" className="item">
@@ -46,7 +36,7 @@ function App() {
         </div>
         <div className="main">
           <Switch>
-            <Route exact path="/portfolio" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
             <Route path="/contact" component={Contact} />
@@ -64,7 +54,7 @@ function App() {
           </a>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
